cc0d29016ce79d1055b657248bc363840c4bedee
Shinsuke Nishio <nishio@densan-labs.net>
2017-06-09 00:28:02 +0900

    setup rails application config

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,16 @@ Bundler.require(*Rails.groups)
 
 module SakilaShop
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+
+    config.autoload_paths += Dir["#{config.root}/services"]
+
+    config.generators do |g|
+      g.helper false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
